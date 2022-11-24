b44311bcd1f3a364e3a4fb5f520e4447b6a99e24
oussamablk-1 <78028622+oussamablk-1@users.noreply.github.com>
2022-11-24 16:37:50 +0100

    Update projects.jsx

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -6,6 +6,15 @@ const Projects = () => {
     <section id='projects'>
       <h5>Mes récents travaux</h5>
       <h2>Projets</h2>
+
+      <div className="container projects__container">
+        <article className='project__item'>
+          <div className="project__item-img">
+            <h3>Einhar</h3>
+            <a href=""></a>
+          </div>
+        </article>
+      </div>
     </section>
   )
 }
