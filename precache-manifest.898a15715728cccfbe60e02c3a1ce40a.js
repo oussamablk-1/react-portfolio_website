ba56061efadcec56a43c234454469b72dc53c875
oussamablk-1 <oussama.belkadi72@gmail.com>
2022-12-17 17:19:28 +0100

    Updates

---
+++ precache-manifest.898a15715728cccfbe60e02c3a1ce40a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e92285bc3a45af8b696e",
+    "url": "/react-portfolio_website/static/css/main.d7b6cce6.chunk.css"
+  },
+  {
+    "revision": "e92285bc3a45af8b696e",
+    "url": "/react-portfolio_website/static/js/main.e92285bc.chunk.js"
+  },
+  {
+    "revision": "c85c660b2f0c176c50cd",
+    "url": "/react-portfolio_website/static/js/1.c85c660b.chunk.js"
+  },
+  {
+    "revision": "3d3b79da9a712a51a93f",
+    "url": "/react-portfolio_website/static/js/runtime~main.3d3b79da.js"
+  },
+  {
+    "revision": "337049711862b0db31cf10f8c59d25f9",
+    "url": "/react-portfolio_website/static/media/CV_Belkadi_Oussama.33704971.pdf"
+  },
+  {
+    "revision": "aac4dc36aed90571ac643aff41cf0b9b",
+    "url": "/react-portfolio_website/static/media/moi.aac4dc36.png"
+  },
+  {
+    "revision": "807aee153a16723b8805a4b63df504d2",
+    "url": "/react-portfolio_website/static/media/moiGrand.807aee15.jpg"
+  },
+  {
+    "revision": "55d3d4bb03c8900d93bc42ba63d409b2",
+    "url": "/react-portfolio_website/static/media/einhar.55d3d4bb.png"
+  },
+  {
+    "revision": "c79caec3e69b7baff7ded8f422fa1c14",
+    "url": "/react-portfolio_website/static/media/portfolio.c79caec3.png"
+  },
+  {
+    "revision": "fef547a872ed22630228276efdec2355",
+    "url": "/react-portfolio_website/static/media/WCS.fef547a8.png"
+  },
+  {
+    "revision": "6f53ed47860e98d3dea74c3867db6c61",
+    "url": "/react-portfolio_website/static/media/kurokabe.6f53ed47.png"
+  },
+  {
+    "revision": "e88e845afe03b99ca15b1daad86cb5ef",
+    "url": "/react-portfolio_website/index.html"
+  }
+];
